6425024a668ffbeda0d681c5fd47b5b41dfa6aa0
shishir0611 <93257735+shishir0611@users.noreply.github.com>
2023-04-22 13:23:14 +0530

    Add files via upload

---
+++ src/cleanup.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+ECHO ===========================Shashank Ramesh - PES1UG20CS392===========================
+ECHO ===========================Shishir Hegde - PES1UG20CS396===========================
+ECHO ===========================Shriya Sastry- PES1UG20CS414===========================
+ECHO ===========================Siddharth Soora- PES1UG20CS426===========================
+
+@ECHO OFF
+ECHO Removing images,pods and deployments...
+kubectl delete pod --all
+kubectl delete deploy --all
+docker rmi -f blogapp:1.0
+
+@ECHO OFF
+ECHO --------Cleaned up Successfully-----------
+
+pause
